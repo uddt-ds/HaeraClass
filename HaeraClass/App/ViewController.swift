34607c9787a9b280ab139df83d3d7b2a3adf351f
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-02 16:26:15 +0900

    [Chore] initial commit

---
+++ HaeraClass/App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HaeraClass
+//
+//  Created by Lee on 9/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
