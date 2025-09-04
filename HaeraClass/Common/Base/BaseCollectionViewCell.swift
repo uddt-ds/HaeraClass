04615e27579bdc2e3f8d4a498b9e086fc47cc406
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-04 19:39:57 +0900

    [Feat] BaseCollectionViewCell 구현

---
+++ HaeraClass/Common/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  BaseTableViewCell.swift
+//  HaeraClass
+//
+//  Created by Lee on 9/2/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func configureHierarchy() {
+
+    }
+
+    func configureLayout() {
+
+    }
+
+    func configureView() {
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
+    }
+
+}
