a2259a284730a6775a0146e3bddb1c8dc9f166db
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-11 21:36:22 +0900

    [Refactor] DTO 모델 생성

--- HaeraClass/ClassCheck/Model/ClassCheck.swift
+++ HaeraClass/ClassCheck/Model/ClassCheck.swift
@@ -2,88 +2,23 @@
 //  ClassCheck.swift
 //  HaeraClass
 //
-//  Created by Lee on 9/2/25.
+//  Created by Lee on 9/11/25.
 //
 
 import Foundation
 
-struct ClassCheck: Decodable {
-    let data: [Data]
-}
-
-struct Data: Decodable {
-    let classId: String
-    let category: Int
+struct ClassCheck {
+    let categoryTitle: String
+    let category: Int  // UI 미사용 : 필터용
     let title: String
     let description: String
-    let price: Int?
-    let salePrice: Int?
-    let imageUrl: String
-    let createdAt: String
+    let price: String
+    let salePrice: String
+    let createdAt: String // UI 미사용 : 정렬용
+    let imageUrl: URL?
+    let persent: String
     let isLiked: Bool
     let creator: Creator
-
-    enum CodingKeys: String, CodingKey {
-        case classId = "class_id"
-        case category
-        case title
-        case description
-        case price
-        case salePrice = "sale_price"
-        case imageUrl = "image_url"
-        case createdAt = "created_at"
-        case isLiked = "is_liked"
-        case creator
-    }
 }
 
-extension Data {
-    var bindImageUrl: URL? {
-        return URL(string: BaseURL.url + "/v1" + imageUrl)
-    }
-
-    var categoryTitle: String {
-        return CategoryTitle(rawValue: category)?.title ?? ""
-    }
-
-    var bindSalePrice: String {
-        guard let salePrice else {
-            return "무료"
-        }
-        return salePrice.demical
-    }
-
-    var bindPrice: String {
-        guard let price else {
-            return "무료"
-        }
-        return price.demical
-    }
-
-    var persent: String {
-        if let price, let salePrice {
-            return "\((salePrice * 100) / price)%"
-        }
-        return ""
-    }
-}
-
-struct Creator: Decodable {
-    let userID: String
-    let nick: String
-    let profileImage: String?
-
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case nick
-        case profileImage
-    }
-}
-
-extension Creator {
-    var bindImageUrl: URL? {
-        return URL(string: BaseURL.url + "/v1" + (profileImage ?? ""))
-    }
-}
-
-
+// 
---
+++ HaeraClass/ClassCheck/Model/ClassCheckDTO.swift
@@ -0,0 +1,103 @@
+//
+//  ClassCheck.swift
+//  HaeraClass
+//
+//  Created by Lee on 9/2/25.
+//
+
+import Foundation
+
+struct ClassCheckDTO: Decodable {
+    let data: [Data]
+}
+
+struct Data: Decodable {
+    let classId: String
+    let category: Int
+    let title: String
+    let description: String
+    let price: Int?
+    let salePrice: Int?
+    let imageUrl: String
+    let createdAt: String
+    let isLiked: Bool
+    let creator: Creator
+
+    enum CodingKeys: String, CodingKey {
+        case classId = "class_id"
+        case category
+        case title
+        case description
+        case price
+        case salePrice = "sale_price"
+        case imageUrl = "image_url"
+        case createdAt = "created_at"
+        case isLiked = "is_liked"
+        case creator
+    }
+
+    func toDomain() -> ClassCheck {
+        return .init(categoryTitle: categoryTitle,
+                     category: category,
+                     title: title,
+                     description: description,
+                     price: bindPrice,
+                     salePrice: bindSalePrice,
+                     createdAt: createdAt,
+                     imageUrl: bindImageUrl,
+                     persent: persent,
+                     isLiked: isLiked,
+                     creator: creator)
+    }
+}
+
+extension Data {
+    var bindImageUrl: URL? {
+        return URL(string: BaseURL.url + "/v1" + imageUrl)
+    }
+
+    var categoryTitle: String {
+        return CategoryTitle(rawValue: category)?.title ?? ""
+    }
+
+    var bindSalePrice: String {
+        guard let salePrice else {
+            return "무료"
+        }
+        return salePrice.demical
+    }
+
+    var bindPrice: String {
+        guard let price else {
+            return "무료"
+        }
+        return price.demical
+    }
+
+    var persent: String {
+        if let price, let salePrice {
+            return "\((salePrice * 100) / price)%"
+        }
+        return ""
+    }
+}
+
+struct Creator: Decodable {
+    let userID: String
+    let nick: String
+    let profileImage: String?
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "user_id"
+        case nick
+        case profileImage
+    }
+}
+
+extension Creator {
+    var bindImageUrl: URL? {
+        return URL(string: BaseURL.url + "/v1" + (profileImage ?? ""))
+    }
+}
+
+
