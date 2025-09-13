b4be692262a1c45184f601c83751198072fccb04
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-13 10:55:50 +0900

    [Feat] ClassDetailDTO 구현, ClassDetail 모델(바인딩 전용 모델) 구현

--- HaeraClass/ClassDetail/Model/ClassDetail.swift
+++ HaeraClass/ClassDetail/Model/ClassDetail.swift
@@ -1,40 +1,18 @@
 //
-//  ClassDetailModel.swift
+//  ClassDetail.swift
 //  HaeraClass
 //
-//  Created by Lee on 9/4/25.
+//  Created by Lee on 9/12/25.
 //
 
 import Foundation
 
-struct ClassDetail: Decodable {
-    let classId: String
-    let category: Int
-    let title: String
+struct ClassDetail {
     let description: String
-    let price: Int?
-    let salePrice: Int?
-    let location: String?
-    let date: String?
-    let capacity: Int?
-    let imageUrls: [String]
-    let createdAt: String
+    let location: String
+    let date: String
+    let capacity: String
     let isLiked: Bool
-    let creator: Creator
-
-    enum CodingKeys: String, CodingKey {
-        case classId = "class_id"
-        case category
-        case title
-        case description
-        case price
-        case salePrice = "sale_price"
-        case location
-        case date
-        case capacity
-        case imageUrls = "image_urls"
-        case createdAt = "created_at"
-        case isLiked = "is_liked"
-        case creator
-    }
+    let profileImage: URL?
+    let creatorNick: String
 }
---
+++ HaeraClass/ClassDetail/Model/ClassDetailDTO.swift
@@ -0,0 +1,73 @@
+//
+//  ClassDetailModel.swift
+//  HaeraClass
+//
+//  Created by Lee on 9/4/25.
+//
+
+import Foundation
+
+struct ClassDetailDTO: Decodable {
+    let classId: String
+    let category: Int
+    let title: String
+    let description: String
+    let price: Int?
+    let salePrice: Int?
+    let location: String?
+    let date: String?
+    let capacity: Int?
+    let imageUrls: [String]
+    let createdAt: String
+    let isLiked: Bool
+    let creator: Creator
+
+    enum CodingKeys: String, CodingKey {
+        case classId = "class_id"
+        case category
+        case title
+        case description
+        case price
+        case salePrice = "sale_price"
+        case location
+        case date
+        case capacity
+        case imageUrls = "image_urls"
+        case createdAt = "created_at"
+        case isLiked = "is_liked"
+        case creator
+    }
+
+    func makeToEntity() -> ClassDetail {
+        return .init(description: description,
+                     location: bindLocation,
+                     date: bindDate,
+                     capacity: bindCapacity,
+                     isLiked: isLiked,
+                     profileImage: creator.bindImageUrl,
+                     creatorNick: creator.nick)
+    }
+}
+
+extension ClassDetailDTO {
+    var bindLocation: String {
+        return location ?? "미정"
+    }
+
+    var bindDate: String {
+        return DateManager.setupDate(value: date)
+    }
+
+    var bindCapacity: String {
+        return changeValue(capacity)
+    }
+
+    // TODO: 다른 방법 또 있는지 찾아보기
+    private func changeValue(_ capacity: Int?) -> String {
+        if capacity == nil {
+            return "미정"
+        } else {
+            return "\(capacity!.demical)명"
+        }
+    }
+}
