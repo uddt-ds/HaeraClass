3ce64abe604fbe68d4bd53b01a6f4ee3471a5153
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-15 08:59:41 +0900

    [Fix] 텍스트 오류

--- HaeraClass/ClassCheck/View/SortButton.swift
+++ HaeraClass/ClassCheck/View/SortButton.swift
@@ -12,8 +12,8 @@ import RxCocoa
 final class SortButton: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setTitle("최신순", for: .normal)
-        setTitle("금액 높은순", for: .selected)
+        setTitle("금액 높은순", for: .normal)
+        setTitle("최신순", for: .selected)
         titleLabel?.font = .boldSystemFont(ofSize: 12)
         setTitleColor(ColorSet.lightOrange.color, for: .normal)
         setTitleColor(ColorSet.lightOrange.color, for: .selected)
