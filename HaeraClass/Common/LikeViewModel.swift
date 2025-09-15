83b4706950b6b471562560464851769e13fee92c
우다당탕 개발자 <uddt_ds@kakao.com>
2025-09-15 21:20:07 +0900

    [Refactor] LikeViewModel Input / Output 패턴 적용

--- HaeraClass/Common/LikeViewModel.swift
+++ HaeraClass/Common/LikeViewModel.swift
@@ -9,24 +9,26 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-final class LikeViewModel {
-
+final class LikeViewModel: ViewModelProtocol {
     private let networkManager = NetworkManager.shared
     private let disposeBag = DisposeBag()
 
-    private let heartButtonTapped: PublishSubject<(String, Bool)>
-
-    let isLiked = PublishRelay<Bool>()
-    let saveResult = PublishRelay<String>()
-    let errorMessage = PublishRelay<String>()
-
-    init(heartButtonTapped: PublishSubject<(String, Bool)>) {
-        self.heartButtonTapped = heartButtonTapped
-        bind()
+    struct Input {
+        let heartButtonTapped: PublishSubject<(String, Bool)>
     }
 
-    func bind() {
-        heartButtonTapped
+    struct Output {
+        let isLiked: PublishRelay<Bool>
+        let saveResult: PublishRelay<String>
+        let errorMessage: PublishRelay<String>
+    }
+
+    func transform(input: Input) -> Output {
+        let isLiked = PublishRelay<Bool>()
+        let saveResult = PublishRelay<String>()
+        let errorMessage = PublishRelay<String>()
+
+        input.heartButtonTapped
             .withUnretained(self)
             .flatMap { owner, value in
                 let (classId, isLiked) = value
@@ -35,9 +37,9 @@ final class LikeViewModel {
             .bind(with: self) { owner, responseData in
                 switch responseData {
                 case .success(let data):
-                    owner.isLiked.accept(data.likeStatus)
+                    isLiked.accept(data.likeStatus)
                 case .failure(let error):
-                    owner.errorMessage.accept(error.errorMessage)
+                    errorMessage.accept(error.errorMessage)
                 }
             }
             .disposed(by: disposeBag)
@@ -45,9 +47,11 @@ final class LikeViewModel {
         isLiked
             .map { $0 ? Message.isLiked.title : Message.isNotLiked.title }
             .bind(with: self) { owner, value in
-                owner.saveResult.accept(value)
+                saveResult.accept(value)
             }
             .disposed(by: disposeBag)
+
+        return Output(isLiked: isLiked, saveResult: saveResult, errorMessage: errorMessage)
     }
 }
 
